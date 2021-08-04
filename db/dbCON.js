8487b1471ddd8c9de86966ba207bcc22bf11a606
Alfred <85899437+abenford09@users.noreply.github.com>
2021-08-04 13:48:50 -0500

    updated

--- db/dbCON.js
+++ db/dbCON.js
@@ -3,6 +3,7 @@ const { Pool } = require('pg')
 const pool = new Pool({
 
     connectionString: process.env.DATABASE_URL,
+    host: 'https://evening-oasis-51166.herokuapp.com/',
     ssl: {
         rejectUnauthorized: false
     }
--- public/main.js
+++ public/main.js
@@ -3,8 +3,17 @@ console.log("working client still, google font is in")
 getAlldata()
 
 async function getAlldata() {
-    $.get('https://evening-oasis-51166.herokuapp.com/', function(data) {
-        // Console log the data to see what you get back and how to use it.
-        console.log(data) // arr of objs
-    })
+    const res = await fetch('https://evening-oasis-51166.herokuapp.com/api/exercise')
+    const data = await res.json()
+    console.log(data)
+    paintScreen(data)
+}
+
+function paintScreen(data) {
+    for (let i = 0; i < data.length; i++) {
+        let current = data[i]
+        const div = document.createElement('div')
+        div.textContent = current.name
+        container.appendChild(div)
+    }
 }
